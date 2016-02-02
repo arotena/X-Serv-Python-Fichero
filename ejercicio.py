a5cbc238b55d0d6df12b4793607d0b1f722f4394
alberto rodriguez tena <atena@zeta40.aulas.gsyc.es>
2016-02-02 12:47:12 +0100

    He realizado el ejercicio

--- ejercicio.py
+++ ejercicio.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+fd = open("/etc/passwd","r");
+lineas = fd.readlines()
+fd.close()
 
+for linea in lineas:
+    if not linea:
+        break
+    corte = linea.split(':')
+    login = corte[0] 
+    shell = corte[-1]
+    print login, shell[:-1]
+    
+   
